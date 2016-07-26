c224a1713cb998d1eab8e868b3f07b1d64d6dcb9
Tristan Quaife <tquaife@gmail.com>
2016-07-26 09:55:39 +0100

    added license

--- sellersTwoStream.py
+++ sellersTwoStream.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
+""" sellersTwoStream.py
 
+Implements the Sellers two-stream model of radiative transfer in
+vegetation canopies. Capable of being set up to mimic the calculations 
+in JULES and CLM as well as having generic functions to allow the use 
+of arbitrary leaf angular distributions and "structure factors" 
+(after Pinty et al. (2006)).
+    
+Copyright (C) 2016 Tristan Quaife
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Tristan Quaife
+tquaife@gmail.com
+"""
 import sys
 
 import numpy as np
